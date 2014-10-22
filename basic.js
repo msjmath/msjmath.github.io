c230cb960b55f2c0c0bb2107f29952a07f586bf3
qwerytiop <qwerytiop@gmail.com>
2014-10-21 18:36:39 -0700

    hello

--- basic.js
+++ basic.js
@@ -3,7 +3,7 @@ var header=
     '<div style="position:relative;left:7%;top:20px;width:150px;height:60px;overflow:hidden;">'+
       '<img src="logo.png" style="height:75px;width:150px;"/>'+
     '</div>'+
-    '<ul style="position:absolute;right:7%;top:34px;width:648px;height:auto;overflow:hidden;background:red;">'+
+    '<ul style="position:absolute;right:7%;top:33px;width:648px;height:auto;overflow:hidden;background:red;z-index:91;">'+
       '<li><a class="nav" href="index.html">Home</a></li>'+
       '<li><a class="nav" href="lectures.html">Lectures</a></li>'+
       '<li><a class="nav" href="events.html">Events</a></li>'+
@@ -11,7 +11,6 @@ var header=
       '<li><a class="nav" href="magmar.html">MagMaR</a></li>'+
       '<li><a class="nav" href="contact.html">Contact</a></li>'+
     '</ul>'+
-    '<div class="line"></div>'+
   '</div>';
 
 var footer=''
