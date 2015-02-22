872d243557b06b7adc578c48e87ddd71d5f5ed4b
qwerytiop <qwerytiop@gmail.com>
2015-02-22 12:44:12 -0800

    mrow

--- basic.js
+++ basic.js
@@ -3,13 +3,13 @@ var header=
     '<div style="position:relative;left:7%;top:20px;width:150px;height:60px;overflow:hidden;">'+
       '<img src="logo.png" style="height:75px;width:150px;"/>'+
     '</div>'+
-    '<ul style="position:absolute;right:7%;top:33px;width:648px;height:auto;overflow:hidden;background:red;z-index:91;">'+
-      '<li><a class="nav" href="index.html">Home</a></li>'+
-      '<li><a class="nav" href="lectures.html">Lectures</a></li>'+
-      '<li><a class="nav" href="events.html">Events</a></li>'+
-      '<li><a class="nav" href="mmt.html">MMT</a></li>'+
-      '<li><a class="nav" href="magmar.html">MagMaR</a></li>'+
-      '<li><a class="nav" href="contact.html">Contact</a></li>'+
+    '<ul class="nav" style="position:absolute;right:7%;top:33px;width:648px;height:auto;overflow:hidden;background:red;z-index:91;">'+
+      '<li class="nav"><a class="nav" href="index.html">Home</a></li>'+
+      '<li class="nav"><a class="nav" href="lectures.html">Lectures</a></li>'+
+      '<li class="nav"><a class="nav" href="events.html">Events</a></li>'+
+      '<li class="nav"><a class="nav" href="mmt.html">MMT</a></li>'+
+      '<li class="nav"><a class="nav" href="magmar.html">MagMaR</a></li>'+
+      '<li class="nav"><a class="nav" href="contact.html">Contact</a></li>'+
     '</ul>'+
     '<div class="line"></div>'+
   '</div>';
